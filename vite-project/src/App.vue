1e078a5c1050eddda9eba0012b2d12983c23a285
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-11-30 18:51:53 +0100

    first commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,23 @@
+
+<script>
+import axios from 'axios';
+import { store } from "./store.js"
+import Birrerie from './components/Birrerie.vue'
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  mounted() {
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped></style>
